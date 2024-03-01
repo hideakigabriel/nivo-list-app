548761e12225f19f27bd75db232ae06ca167e7d4
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-01 00:31:16 -0300

    feat: add new tag form base ui

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,17 +1,29 @@
+import { Check, X } from "lucide-react";
+import { Button } from "./ui/button";
+
 export function CreateTagForm() {
   return (
-    <form action="">
-      
-      <div>
-        <label>Tag name</label>
-        <input type="text" name="" id="" />
-      </div>
-      
-      <div>
-        <label>Slug</label>
-        <input type="text" name="" id="" />
+    <form className="w-full space-y-6">
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="name">Tag name</label>
+        <input id="name" type="text" name="" className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm" />
       </div>
 
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="slug">Slug</label>
+        <input id="slug" type="text" readOnly className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm" />
+      </div>
+
+      <div className="flex items-center justify-end ml-auto gap-2">
+        <Button>
+          <X className="size-3" />
+          Cancel
+        </Button>
+        <Button type="submit" className="bg-teal-400 text-teal-950">
+          <Check className="size-3" />
+          Save
+        </Button>
+      </div>
     </form>
-  )
+  );
 }
